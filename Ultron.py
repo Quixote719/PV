446be3d74d0d383a1a67b72e847eb060445b91de
Daniel <lwang41@jaguarlandrover.com>
2023-10-19 17:49:23 +0800

    Ultron

---
+++ Ultron.py
@@ -0,0 +1,31 @@
+a = 7
+a = a + a
+print(a)
+
+s = 'Hello World'
+print(s, s[0], s[3])
+
+x, y = 'some', 'more'
+print("I'm going to inject %s text here, and %s text here."%(x,y))
+
+my_list = ['one','two','three',4,5]
+print(my_list[1], my_list[3])
+
+my_dict = {'key1':'value1','key2':'value2'}
+print(my_dict['key1'], my_dict['key2'])
+
+t = ('one', 2, 'III', 2)
+print(t[0],t[1],t[-1],t[-2])
+print('index of turple', t.index(2), t.count(2))
+
+list1 = [1,1,2,2,3,4,5,6,1,1]
+s1 = set(list1)
+print(s1)
+
+bl1 = True; objN = None
+print(bl1, 1>2)
+print(objN)
+objN = 3
+print(objN)
+# objN = {a:1}
+# print(objN)
